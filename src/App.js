6360e169bf7680fb1bd074339a1d95ed71f2c54d
TimeSourceSheep <timesourcesheep@gmail.com>
2020-07-27 23:55:36 +0900

    20200727 updated react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Dino({fav}){
+  return <h3>I just tamed {fav}!!!</h3>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div>
+     <h1>Hell was here.</h1>
+     <Dino fav="Parasaurolophus" />
+   </div>
+  )
 }
 
 export default App;
