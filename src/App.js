6347f7c48b8ca0663248776016d2904348548e2d
TimeSourceSheep <timesourcesheep@gmail.com>
2020-08-31 23:58:20 +0900

    Update20200831: Show movies now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
     state = {
         isLoading: true,
-        data: [],
+        movies: [],
     };
+    getMovies = async() => {
+        const {
+            data:{
+                data:{movies},
+            },
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+        this.setState({movies,isLoading:false});
+    }
     componentDidMount(){
-        //Here goes loading code
+        this.getMovies();
     }
     render(){
-        const {isLoading}=this.state;
-        return <div>{isLoading?'Loading...':'Loading Complete.'}</div>;
+        const {isLoading,movies}=this.state;
+        return <section class="container">
+            {isLoading ? (
+                <div class="loader">
+                    <span class="loader__text">Loading...</span>
+                </div>
+            ):(
+                <div class="movies">
+                                {movies.map(
+                    (movie)=>{
+                    return <Movie
+                        key={movie.id}
+                        id={movie.id}
+                        year={movie.year}
+                        title={movie.title}
+                        summary={movie.summary}
+                        poster={movie.medium_cover_image}
+                    />;
+                    })}
+                </div>
+            )
+            
+
     }
 }
 
