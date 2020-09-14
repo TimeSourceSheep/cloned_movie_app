5c7aeb807cc1b39bf5292a5852467c847e5588c8
TimeSourceSheep <timesourcesheep@gmail.com>
2020-09-14 10:13:28 +0900

    Local file update

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>"Me smash"</span>
+      <span>- An Orc, unknown</span>
+    </div>
+  );
+}
+
+export default About;
